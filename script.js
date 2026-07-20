300c8cd6e6c6ce58e39464d8ca2401300130872e
Arthurtech8 <iarthurti@gmail.com>
2026-07-20 20:16:17 -0300

    Algoritmos ex

--- script.js
+++ script.js
@@ -1,8 +1,32 @@
-// const nome = arthur
-// const sobrenome = pires
-// const templateString = `Olá meu nome completo é: ${nome}${sobrenome}`
 
-const nome = Arthur 
-const sobrenome = Pires
-const saudacao = Olá, meu nome completo é 
+const nome = "Arthur" 
+const sobrenome = "Pires"
+const saudacao = "Olá, meu nome completo é:"
+const nomeCompleto = saudacao + " " + nome + " " + sobrenome
 
+console.log(nomeCompleto)
+
+const templateString = `Olá, meu nome completo é ${nome} ${sobrenome}`
+
+console.log(templateString)
+
+const numero1 = 2026
+const numero2 = 2002
+const resultado = numero1 - numero2
+console.log("Tenho",resultado,"anos")
+
+const numero3 = 4
+console.log(numero3 % 2)
+if(numero3 % 2 == 0){
+   // é numeral par
+    console.log(numero3," é par")
+}
+else{
+   // é numeral impar
+   console.log(numero3," é impar")
+}
+
+const valor = 400
+const valorDesconto = valor - (valor * 0.10)
+
+console.log(`Oferta de bicicletas, de ${valor} por ${valorDesconto} no PIX`)
