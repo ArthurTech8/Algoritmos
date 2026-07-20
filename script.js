156bd2d251a658ead7ccffc2c2d8f47b9893b416
Arthurtech8 <iarthurti@gmail.com>
2026-07-20 19:40:05 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,8 @@
+// const nome = arthur
+// const sobrenome = pires
+// const templateString = `Olá meu nome completo é: ${nome}${sobrenome}`
+
+const nome = Arthur 
+const sobrenome = Pires
+const saudacao = Olá, meu nome completo é 
+
