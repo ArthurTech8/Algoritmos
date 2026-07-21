a7b5e87b74aca131276f01818ad9567c6ff10ca1
Arthurtech8 <iarthurti@gmail.com>
2026-07-21 20:50:06 -0300

    Else if - multiplos - imc - desconto

--- script2.js
+++ script2.js
@@ -1,10 +1,10 @@
-// const produtoDigitado = prompt("Digite o valor do produto")
-// const produto = parseInt(produtoDigitado)
-// const descontoDigitado = prompt("Digite a porcentagem do desconto")
-// const desconto = parseFloat(descontoDigitado)
+const produtoDigitado = prompt("Digite o valor do produto")
+const produto = parseInt(produtoDigitado)
+const descontoDigitado = prompt("Digite a porcentagem do desconto")
+const desconto = parseFloat(descontoDigitado)
 
-// const valorFinal = produto - (produto * (desconto / 100))
-// alert(`O valor do produto é ${produto} com desconto de ${desconto}% fica ${valorFinal}`)
+const valorFinal = produto - (produto * (desconto / 100))
+alert(`O valor do produto é ${produto} com desconto de ${desconto}% fica ${valorFinal}`)
 
 
 const pesoDigitado = prompt("Digite o seu peso(Kg):")
@@ -15,6 +15,42 @@ const imcFinal = peso / (altura * altura)
 
 alert(`Seu IMC é de ${imcFinal.toFixed(2)}`)
 
-if(imcFinal < 18.5){
-    alert(`Seu imc é ${imcFinal.toFixed(2)}.\nClassificação abaixo do peso`)
+if(imcFinal >=18.5 && imcFinal <25){
+    alert(`Seu imc é ${imcFinal.toFixed(2)}.\nClassificação de Peso: Normal`)
+}
+else if(imcFinal >=25 && imcFinal <30){
+    alert(`Seu imc é ${imcFinal.toFixed(2)}.\nClassificação de Peso: Sobrepeso`)
+}
+else if(imcFinal >=30 && imcFinal <35){
+    alert(`Seu imc é ${imcFinal.toFixed(2)}.\nClassificação de Peso: Obeso classe 1`)
+}
+else if(imcFinal >=35 && imcFinal <40){
+    alert(`Seu imc é ${imcFinal.toFixed(2)}.\nClassificação de Peso: Obeso classe 2`)
+}
+else{
+    alert(`Seu imc é ${imcFinal.toFixed(2)}.\nClassificação de Peso: Obeso classe 3`)
+}
+
+
+
+const numero3 = prompt("Digite um número");
+const numero = parseInt(numero3);
+
+if (numero % 2 == 0 && numero % 3 == 0 && numero % 5 == 0) {
+    alert(`O número ${numero} é par e múltiplo de 3 e 5.`);
+}
+else if (numero % 2 == 0) {
+    alert(`O número ${numero} é par.`);
+}
+else if (numero % 3 == 0 && numero % 5 == 0) {
+    alert(`O número ${numero} é múltiplo de 3 e de 5.`);
+}
+else if (numero % 3 == 0) {
+    alert(`O número ${numero} é múltiplo de 3.`);
+}
+else if (numero % 5 == 0) {
+    alert(`O número ${numero} é múltiplo de 5.`);
+}
+else {
+    alert(`O número ${numero} é ímpar e não é múltiplo de 3 nem de 5.`);
 }
