57ef6e43178091cffc1b6707b959694cb7054f43
Arthurtech8 <iarthurti@gmail.com>
2026-07-23 20:22:11 -0300

    Palavra

--- script3.js
+++ script3.js
@@ -1,18 +1,18 @@
-let valorEmprestimo = prompt("Qual o valor do emprestimo: ")
-let emprestimo = parseInt(valorEmprestimo)
-let valorParcelas = prompt("Em quantas parcelas gostaria: ")
-let parcelas = parseInt(valorParcelas)
-let valorJuros = prompt("Quantos % de juros seria sobre a parcela: ")
-let juros = parseFloat(valorJuros)
+// let valorEmprestimo = prompt("Qual o valor do emprestimo: ")
+// let emprestimo = parseInt(valorEmprestimo)
+// let valorParcelas = prompt("Em quantas parcelas gostaria: ")
+// let parcelas = parseInt(valorParcelas)
+// let valorJuros = prompt("Quantos % de juros seria sobre a parcela: ")
+// let juros = parseFloat(valorJuros)
 
-let parcelaValor = emprestimo / parcelas;
-    alert(`O valor da parcela é de ${parcelaValor}`);
-let jurosValor = parcelaValor * (juros / 100);
-    alert(`O valor do juros sobre a parcela é ${jurosValor}`);
-let parcelaJuros = parcelaValor + jurosValor;
-    alert(`O valor da parcela com juros é de ${parcelaJuros}`);
-let valorTotal = parcelaJuros * parcelas;
-    alert(`O total do emprestimo seria de ${valorTotal}`);
+// let parcelaValor = emprestimo / parcelas;
+//     alert(`O valor da parcela é de ${parcelaValor}`);
+// let jurosValor = parcelaValor * (juros / 100);
+//     alert(`O valor do juros sobre a parcela é ${jurosValor}`);
+// let parcelaJuros = parcelaValor + jurosValor;
+//     alert(`O valor da parcela com juros é de ${parcelaJuros}`);
+// let valorTotal = parcelaJuros * parcelas;
+//     alert(`O total do emprestimo seria de ${valorTotal}`);
 
 //prompt parcelas
 //prompt juros parcela
@@ -20,27 +20,46 @@ let valorTotal = parcelaJuros * parcelas;
 //1050 * 10 = total
 // 1050 - parcela
 
+let palavra = prompt("Digite uma palavra: ");
+let vogais = "";
+let contador = 0
 
+let palavraInvertida= " ";
 
-
-
-
-let nomeDigitado = prompt("Digite uma frase:");
-
-let nomeAlternado = "";
-
-for (let i = 0; i < nomeDigitado.length; i++) {
-
-    if (i % 2 == 0) {
-        nomeAlternado += nomeDigitado[i].toUpperCase();
-    } else {
-        nomeAlternado += nomeDigitado[i].toLowerCase();
+for(let i =0; i < palavra.length; i++){
+    let letra = palavra[i].toLowerCase();
+    
+    if ("aeiou".includes(letra)){
+        vogais += letra + " ";
+        contador++
     }
-
 }
+for(let j= palavra.length -1; j>= 0; j--){
+    palavraInvertida += palavra[j].toLowerCase();
+}
+alert(`A palavra possui ${palavra.length} letras`)
+alert(`A palavra ${palavra} possui ${contador} vogais, sendo as vogais ${vogais}`)
+alert(`E o inverso desta palavra é ${palavraInvertida}`)
 
-alert(`Nome original: ${nomeDigitado}`);
-alert(`Nome alternado: ${nomeAlternado}`);
+
+
+
+// let nomeDigitado = prompt("Digite uma frase:");
+
+// let nomeAlternado = "";
+
+// for (let i = 0; i < nomeDigitado.length; i++) {
+
+//     if (i % 2 == 0) {
+//         nomeAlternado += nomeDigitado[i].toUpperCase();
+//     } else {
+//         nomeAlternado += nomeDigitado[i].toLowerCase();
+//     }
+
+// }
+
+// alert(`Nome original: ${nomeDigitado}`);
+// alert(`Nome alternado: ${nomeAlternado}`);
 
 // const nome = "Arthur Pires"
 // const nomeSemEspaço = nome
